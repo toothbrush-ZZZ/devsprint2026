db5548a16a356b5eaa5beee943879e50f2cf7198
toothbrush <zeffathzourin@iut-dhaka.edu>
2026-03-02 01:23:10 +0600

    modified identity-provider

--- identity-provider/users/urls.py
+++ identity-provider/users/urls.py
@@ -5,6 +5,8 @@ urlpatterns = [
     path('register/', views.register),
     path('login/', views.login),
     path('verify/', views.verify_token),
+    path('change-password/', views.change_password),
+    path('reset-password/', views.admin_reset_password),
     path('health/', views.health),
     path('metrics/', views.metrics),
     path('chaos/', views.toggle_chaos, name='chaos_toggle'),
